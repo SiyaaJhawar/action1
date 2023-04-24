ff64ca1223b1a9b6314f3960ee8f5fe87493ef5d
Gupta <swgu@softwareag.com>
2023-04-24 14:14:14 +0530

    somemsg

---
+++ PullRequestaddlabel/pullrequest.js
@@ -0,0 +1,28 @@
+// Set up the API endpoint and authentication
+const apiUrl = "https://api.github.com";
+const owner = "SiyaaJhawar";
+const repo = "action1";
+const pull_number = 2; // Replace with the pull request number
+const label = "release";
+const authToken = "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";
+const headers = {
+  Authorization: `token ${authToken}`,
+  Accept: "application/vnd.github.v3+json",
+};
+
+// Add the label to the pull request using fetch
+fetch(`${apiUrl}/repos/${owner}/${repo}/issues/${pull_number}/labels`, {
+  method: "POST",
+  headers,
+  body: JSON.stringify([label]),
+})
+  .then((response) => {
+    if (response.ok) {
+      console.log("Label added to pull request.");
+    } else {
+      console.error("Error adding label to pull request:", response.statusText);
+    }
+  })
+  .catch((error) => {
+    console.error("Error adding label to pull request:", error);
+  });
