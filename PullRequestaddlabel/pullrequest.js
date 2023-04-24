a67430d3091247716014c038f6d9be57cd5ff4c7
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-24 15:32:28 +0530

    Update pullrequest.js

--- PullRequestaddlabel/pullrequest.js
+++ PullRequestaddlabel/pullrequest.js
@@ -1,19 +1,11 @@
-
 import fetch from 'node-fetch';
-
-// Use the fetch module here
-
-
-
-
-
 
 const apiUrl = "https://api.github.com";
 const owner = "SiyaaJhawar";
 const repo = "action1";
 const pull_number = 2; // Replace with the pull request number
 const label = "release";
-const authToken = "process.env.GITHUB_API_TOKEN";
+const authToken = process.env.GITHUB_API_TOKEN;
 const headers = {
   Authorization: `token ${authToken}`,
   Accept: "application/vnd.github.v3+json",
